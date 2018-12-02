8b0c0881d3283f13c96be98c635e9729ee70918d
GuilhermeCaruso <guilherme.caruso@owldocs.com>
2018-12-02 01:15:48 -0200

    COmeçou

--- bin/gwend.js
+++ bin/gwend.js
@@ -13,22 +13,6 @@ gwendCli.checkArgs()
     for(let x = 0; x < args.length; x++ ){
         gwendCli.execCommand(args[x])
     }
-    // const rl = readline.createInterface({
-    //     input: process.stdin,
-    //     output: process.stdout
-    //   });
-      
-    //   rl.question('What do you think of Node.js? ', (answer) => {
-    //     console.log('Thank you for your valuable feedback:', answer);
-    //     rl.close();
-    //   });
-    // exec('git clone https://github.com/GuilhermeCaruso/Gwend EU',(err, stdout, stderr) => {
-    //     if (err) {
-    //       console.error(err);
-    //       return;
-    //     }
-    //     console.log(stdout);
-    //   })
 })
 .catch((e)=> {
     if(e.length == 1) {
--- scripts/create.js
+++ scripts/create.js
@@ -1,3 +1,30 @@
+const readline = require("readline")
+const { exec } = require('child_process');
+
 module.exports = function () {
-    console.log("oi")
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question('Project Name: ', (name) => {
+        rl.question('Version: ', (version) =>{
+            console.log("> Creating project...")
+            exec(`git clone https://github.com/GuilhermeCaruso/Gwend ${name}`, (err, stdout, stderr)=>{
+                if (err) {
+                    console.log(err)
+                    return
+                }else {
+                    console.log("> Installing dependecies...")
+                    exec(`cd ${name} && npm install`, (err, stdout, stderr)=>{
+                        if (err){
+                            console.log(err)
+                        }else {
+                            console.log(`Project ${name} - V.${version} has been created`)
+                        }
+                    })
+                }
+            })            
+            rl.close();
+        })
+    });
 }
